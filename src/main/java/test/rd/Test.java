4d3886231bb11ca5ef6f2d4cefd9f6cbbcb7d6bf
Greg Schueler <greg.schueler@gmail.com>
2020-03-07 18:04:15 -0800

    Cleanup input handling and exit codes

--- src/main/java/test/rd/Test.java
+++ src/main/java/test/rd/Test.java
@@ -12,15 +12,20 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            System.err.println("Args: <RundeckUrl> <AuthToken> <Project>");
+            System.exit(2);
+        }
         Client<RundeckApi> build = RundeckClient.builder().baseUrl(args[0]).tokenAuth(args[1]).build();
         Call<List<JobItem>> listCall = build.getService().listJobs(args[2], null);
         Response<List<JobItem>> execute = listCall.execute();
-        if(execute.isSuccessful()) {
+        if (execute.isSuccessful()) {
             List<JobItem> body = execute.body();
             System.out.println(body.size() + " found:");
             body.forEach(jobItem -> System.out.println(jobItem.getId()));
-        }else{
+        } else {
             System.err.println("Error: " + execute.code() + " " + execute.message());
+            System.exit(2);
         }
     }
 }
