feb56bffe7d9c5d598556c41bb59afc12a74c144
Greg Schueler <greg.schueler@gmail.com>
2020-04-23 09:01:58 -0700

    Add groovy example

--- Readme.md
+++ Readme.md
@@ -5,5 +5,10 @@ This demonstrates using the rd-api-client java library to connect to the [Rundec
 
 The rd-api-client library is located here: <https://github.com/rundeck/rundeck-cli>
 
-and is currently published to bintray here: <https://bintray.com/rundeck/rundeck-maven/rd-api-client>
+and is currently published to bintray here: <https://bintray.com/rundeck/maven/rd-api-client>
+
+
+## Groovy Example
+
+See the `rd-api-test.groovy` file.
 
---
+++ rd-api-test.groovy
@@ -0,0 +1,21 @@
+//@GrabResolver(root = 'https://rundeck.bintray.com/maven',name = 'bintray')//optional for snapshot versions
+@Grab('org.rundeck.api:rd-api-client:1.2.4')
+import org.rundeck.client.RundeckClient
+
+
+if (args.length < 3) {
+    System.err.println("Args: <RundeckUrl> <AuthToken> <Project>")
+    System.exit(2)
+}
+
+def api = RundeckClient.with(args[0],args[1]).build().service
+def call = api.listJobs(args[2], null)
+def execute = call.execute()
+if (execute.successful) {
+    def body = execute.body()
+    println(body.size() + " found:")
+    body.each { jobItem -> println jobItem.id }
+} else {
+    System.err.println("Error: " + execute.code() + " " + execute.message())
+    System.exit(2)
+}
